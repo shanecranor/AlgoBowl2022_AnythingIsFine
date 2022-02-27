fad23fd2b0d447beaf37f7259472ae46632b5b00
Uroy01 <89428310+Uroy01@users.noreply.github.com>
2022-02-27 15:05:35 -0700

    Update output_verification.py
    inputfile1 was for testing

--- output_verification.py
+++ output_verification.py
@@ -1,6 +1,9 @@
-import numpy as np
 from collections import Counter
-line_seen = set()
+
+
 with open( '/Users/student/Downloads/inputfile1.txt', 'r') as f:
      data = f.read()  
 print(data)
+first_line = f.readline()
+print(first_line)
+print(float(first_line))
