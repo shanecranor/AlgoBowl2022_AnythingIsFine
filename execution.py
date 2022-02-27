64e5817109dd440d4ee46d1687ddd8bb83ceeb15
Uroy01 <89428310+Uroy01@users.noreply.github.com>
2022-02-26 23:32:44 -0700

    Update execution.py
    Made changes. Need to check tomorrow again

--- execution.py
+++ execution.py
@@ -1,7 +1,12 @@
-import time
-start = time.time()
-a = 0
-for i in range(1000):
-    a += (i**100)
-end = time.time()
-print("The time of execution of above program is :", end-start)
+import time as t
+import numpy as np
+t1 = t.process_time()
+start = t.perf_counter()
+for i in np.arange(0, 10**8, 1, dtype=np.int64):
+    k = i * i
+t2 = t.process_time()
+end = t.perf_counter()
+spend_1 = t2 - t1
+spend_2 = end - start
+print(" time.process_time(): {}".format(spend_1))
+print(" time.perf_counter(): {}".format(spend_2))
