a03747fc71bcf3035eddb5f530b162c51b621271
Uroy01 <89428310+Uroy01@users.noreply.github.com>
2022-02-27 14:13:07 -0700

    Update output_verification.py

--- output_verification.py
+++ output_verification.py
@@ -1,38 +1,6 @@
-import importlib
-from importlib.resources import path
-from multiprocessing.spawn import import_main_path
-import os
-from pydoc import importfile
-import difflib
-import sys
 import numpy as np
-
-
-
-
-def file_read(fname):
-        content_array = []
-        with open(fname) as f:
-                   
-                for line in f:
-                        content_array.append(line)
-                print(content_array)
-
-
-file_read('test.txt')
-
-initialArray = np.array([file_read('test.txt')])
-  
-
-print("Our Initial Array: ", str(initialArray))
-print("Original type: " + str(type(initialArray[0])))
-  
-
-finalArray = np.asarray(initialArray, dtype = np.float64, 
-                        order ='C')
-  
-
-print("Our Final Array: ", str(finalArray))
-print(list(map(float, initialArray)))
-
-
+from collections import Counter
+line_seen = set()
+with open( '/Users/student/Downloads/inputfile1.txt', 'r') as f:
+     data = f.read()  
+print(data)
